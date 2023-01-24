ae0bfff9eff004f575e21433b0c02ba178f5faab
Matthew Wong <matthewcwwong01@gmail.com>
2023-01-23 22:48:19 -0800

    final changes

--- ImageMethods/checkImage.py
+++ ImageMethods/checkImage.py
@@ -2,12 +2,11 @@ from PIL import Image
 import numpy as np
 
 def method(filename):
-    global img
     valid = ('jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG')
 
     try:
         img = Image.open(filename)
+        return np.NaN if img.format not in valid else filename
     except IOError:
         return np.NaN
 
-    return np.NaN if img.format not in valid else filename
